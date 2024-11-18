33c0e34c1d330b9f8ffad620fd8bd367ea33d4ce
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-11-18 21:59:23 +0300

    new languge to learn:)

---
+++ Hi/FirstClass.dart
@@ -0,0 +1,40 @@
+void main() {
+
+print("object");
+/*
+accepts all types var
+same as num accept all nums
+*/
+const pi = "3.14";
+print("pi number is " + pi);
+
+String name = "aip club";
+print("the name of the club is "+name);
+print("the name of the club is $name");
+
+String x = "hi";
+print(x);
+
+num y = 4.5;
+print(y);
+
+int z = 6;
+print(z);
+
+var w = true; 
+print(w);
+
+String age = "33";
+int r = int.parse(age);
+print(r);
+
+//arthmetic
+int a = 4;
+int b = 6;
+print(a+b);
+print(a-b);
+print(a*b);
+print(a/b);
+print(a%b);
+print(a~/b);//القسمه بدون فاصله
+} 
